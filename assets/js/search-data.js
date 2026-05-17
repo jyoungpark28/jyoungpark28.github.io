87c9be0d5fa8915b8b4cf6aa516806ec6c536ba7
jyoungpark28 <jyoungpark28@gmail.com>
2026-05-17 20:34:12 +0000

    Deploying to gh-pages from @ jyoungpark28/jyoungpark28.github.io@79efa44c17e952c02322a576182ab712b5324d96 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-news",
-          title: "news",
+          title: "News",
           description: "",
           section: "Navigation",
           handler: () => {
